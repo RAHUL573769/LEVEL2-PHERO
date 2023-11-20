29bb916a1d4696fbf6274d7abe4f043cc37efc96
Rahul Rudra <rahulrudra665@gmail.com>
2023-11-20 20:41:16 +0600

    night commit

--- MongooseMaster/Express+TypeScript/src/app/app.ts
+++ MongooseMaster/Express+TypeScript/src/app/app.ts
@@ -1,7 +1,13 @@
-import express from "express";
+import e from "express";
+import express, { Request, Response } from "express";
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+app.use(express.text());
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World Changes Ne1!");
+});
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("This is Post Route");
 });
 export default app;
