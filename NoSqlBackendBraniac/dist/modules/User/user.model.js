d0070a3f172c41e5c3f06c326da130aabda729d7
Rahul Rudra <rahulrudra665@gmail.com>
2023-11-27 08:35:20 +0600

    morning commit

--- NoSqlBackendBraniac/dist/modules/User/user.model.js
+++ NoSqlBackendBraniac/dist/modules/User/user.model.js
@@ -1,1 +1,32 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const userSchema = new mongoose_1.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    needsPasswordChange: {
+        type: Boolean,
+        default: true
+    },
+    role: {
+        type: String,
+        enum: ["student", "faculty", "admin"]
+    },
+    status: {
+        type: String,
+        enum: ["in-progress", "blocked"]
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+const UserModel = (0, mongoose_1.model)("user", userSchema);
--- NoSqlBackendBraniac/dist/modules/User/user.validation.js
+++ NoSqlBackendBraniac/dist/modules/User/user.validation.js
@@ -1,1 +1,17 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserValidation = void 0;
+const zod_1 = require("zod");
+const userValidationSchema = zod_1.z.object({
+    id: zod_1.z.string(),
+    password: zod_1.z
+        .string()
+        .max(20, { message: "Password Cannot be more than 20 Characters" }),
+    needsPasswordChange: zod_1.z.boolean().optional(),
+    role: zod_1.z.enum(["student", "faculty", "admin"]),
+    status: zod_1.z.enum(["in-progress", "blocked"]).default("in-progress"),
+    isDeleted: zod_1.z.boolean().optional().default(false)
+});
+exports.UserValidation = {
+    userValidationSchema
+};
