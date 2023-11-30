d8b756aaab1e0c1c3529532d9650092a91fb9243
Rahul Rudra <rahulrudra665@gmail.com>
2023-11-30 15:05:36 +0600

    new cpractise commit

---
+++ Practises/Practise-1/src/server.ts
@@ -0,0 +1,26 @@
+import app from "./app";
+import config from "./config";
+const port = 3000;
+
+const mongoose = require("mongoose");
+
+async function server() {
+  try {
+    mongoose.connect(config.DB_PASS);
+    console.log("Db Connected");
+  } catch (error) {
+    console.log("Db Not Connected");
+  }
+}
+
+server().catch((err) => {
+  console.log(err);
+});
+// const Cat = mongoose.model('Cat', { name: String });
+
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
+
+app.listen(config.PORT, () => {
+  console.log(`Example app listening on port ${config.PORT}`);
+});
