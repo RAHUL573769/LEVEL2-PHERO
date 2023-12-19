11f5e56e9418c8a0e2bfc57f89448a9e43b77d70
Rahul Rudra <rahulrudra665@gmail.com>
2023-12-19 15:14:33 +0600

    filtering strted

--- NoSqlBraiac-2/MurirTinWebsite-Ongoing/murir-tin-tours-and-travels-session-2-transaction-rollback/dist/validations/tour.validations.js
+++ NoSqlBraiac-2/MurirTinWebsite-Ongoing/murir-tin-tours-and-travels-session-2-transaction-rollback/dist/validations/tour.validations.js
@@ -2,10 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TourValidation = void 0;
 const zod_1 = require("zod");
-const createZodSchema = zod_1.z.object({
-    name: zod_1.z.string(),
+const createZodSchema = zod_1.z
+    .object({
+    name: zod_1.z.string().refine((data) => {
+        if (data.length < 5) {
+            return false;
+        }
+    }, {
+        message: 'Name must be less tha n 5 characters',
+    }),
     durationHours: zod_1.z.number().int().positive().min(1),
     //   ratingsAverage: z.number().int().positive().min(1).max(5),
     price: zod_1.z.number().positive(),
+    discountPrice: zod_1.z.number().int().positive().optional(),
+})
+    .refine((data) => {
+    if (data.discountPrice === undefined) {
+        return true;
+    }
+    if (data.discountPrice > data.price) {
+        return false;
+    }
+}, {
+    message: 'Discount Price Must be less than actual price',
 });
 exports.TourValidation = { createZodSchema };
--- NoSqlBraiac-2/MurirTinWebsite-Ongoing/murir-tin-tours-and-travels-session-2-transaction-rollback/src/validations/tour.validations.ts
+++ NoSqlBraiac-2/MurirTinWebsite-Ongoing/murir-tin-tours-and-travels-session-2-transaction-rollback/src/validations/tour.validations.ts
@@ -1,12 +1,36 @@
 import { z } from 'zod'
 
-const createZodSchema = z.object({
-  name: z.string(),
+const createZodSchema = z
+  .object({
+    name: z.string().refine(
+      (data) => {
+        if (data.length < 5) {
+          return false
+        }
+      },
+      {
+        message: 'Name must be less tha n 5 characters',
+      },
+    ),
 
-  durationHours: z.number().int().positive().min(1),
-  //   ratingsAverage: z.number().int().positive().min(1).max(5),
+    durationHours: z.number().int().positive().min(1),
+    //   ratingsAverage: z.number().int().positive().min(1).max(5),
 
-  price: z.number().positive(),
-})
+    price: z.number().positive(),
+    discountPrice: z.number().int().positive().optional(),
+  })
+  .refine(
+    (data) => {
+      if (data.discountPrice === undefined) {
+        return true
+      }
+      if (data.discountPrice > data.price) {
+        return false
+      }
+    },
+    {
+      message: 'Discount Price Must be less than actual price',
+    },
+  )
 
 export const TourValidation = { createZodSchema }
