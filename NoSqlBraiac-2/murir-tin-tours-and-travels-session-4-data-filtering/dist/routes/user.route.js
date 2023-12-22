ad0819d913855181eaef55249d56a990c4cecfbe
Rahul Rudra <rahulrudra665@gmail.com>
2023-12-22 22:03:10 +0600

    new commit

--- NoSqlBraiac-2/murir-tin-tours-and-travels-session-4-data-filtering/dist/routes/user.route.js
+++ NoSqlBraiac-2/murir-tin-tours-and-travels-session-4-data-filtering/dist/routes/user.route.js
@@ -9,9 +9,10 @@ exports.userRoutes = void 0;
 const express_1 = __importDefault(require("express"));
 const user_controller_1 = require("../controllers/user.controller");
 const auth_1 = require("../middlewares/auth");
+// import { checkAuth } from '../middlewares/auth'
 const router = express_1.default.Router();
-router.post('/create-user', user_controller_1.userController.createUser);
-router.get('/', auth_1.checkAuth, user_controller_1.userController.getAllUsers);
+router.post('/create-user', (0, auth_1.checkAuth)(['array']), user_controller_1.userController.createUser);
+router.get('/', user_controller_1.userController.getAllUsers);
 router.get('/:id', user_controller_1.userController.getSingleUser);
 router.patch('/:id', user_controller_1.userController.updateUser);
 router.delete('/:id', user_controller_1.userController.deleteUser);
--- NoSqlBraiac-2/murir-tin-tours-and-travels-session-4-data-filtering/src/routes/user.route.ts
+++ NoSqlBraiac-2/murir-tin-tours-and-travels-session-4-data-filtering/src/routes/user.route.ts
@@ -5,12 +5,37 @@ import { userController } from '../controllers/user.controller'
 import User from '../models/user.model'
 import catchAsyncFunction from '../utils/catchAsync'
 import { NextFunction } from 'express'
-import { checkAuth } from '../middlewares/auth'
+import { USER_ROLE } from '../constants/users.constants'
+// import { checkAuth } from '../middlewares/auth'
+// import { checkAuth } from '../middlewares/auth'
+const checkAuth = (...roles: Array<keyof typeof USER_ROLE>) => {
+  return catchAsyncFunction(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const email = req.body.email
+      const password = req.body.password
+      const name = req.body.name
+      console.log(email, password, name)
+
+      const user = await User.findOne({ email, password })
+      console.log(user)
+      if (!user) {
+        throw new Error('Invalid Email and Password')
+      }
+      if (roles.includes(user.role)) {
+        throw new Error('You are not Authorizes Password')
+      }
+      next()
+    },
+  )
+}
 
 const router = express.Router()
-
-router.post('/create-user', userController.createUser)
-router.get('/', checkAuth, userController.getAllUsers)
+router.post(
+  '/create-user',
+  checkAuth(USER_ROLE.admin, USER_ROLE.user),
+  userController.createUser,
+)
+router.get('/', userController.getAllUsers)
 router.get('/:id', userController.getSingleUser)
 router.patch('/:id', userController.updateUser)
 router.delete('/:id', userController.deleteUser)
