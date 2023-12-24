054f69d74f38a5bbd8531c623caeeec125e51252
Rahul Rudra <rahulrudra665@gmail.com>
2023-12-24 08:59:14 +0600

    jwt started maincourse

--- NoSqlBraiac-2/JWTBYMezbahBhai(Doing)/first-project-first-project-8/src/app/modules/Authentication/auth.services.ts
+++ NoSqlBraiac-2/JWTBYMezbahBhai(Doing)/first-project-first-project-8/src/app/modules/Authentication/auth.services.ts
@@ -1,7 +1,29 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
 import { TLogin } from './auth.interface';
 
 const loginUser = async (payload: TLogin) => {
   console.log(payload);
+
+  //checking if user exists
+  const isUserExists = await User.findOne({ id: payload.id });
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user not Found');
+  }
+  //checking if the user is already deleated
+  const isDeleted = isUserExists?.isDeleted;
+  if (isDeleted) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is Deleted');
+  }
+
+  //checking if the user is blocked
+  const userStatus = isUserExists?.status;
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is Blocked');
+  }
+
+  //Aceess Granted
 };
 
 export const AuthServices = { loginUser };
