557bf5d26ea9af2f76c39220585a99c063f5930d
Rahul Rudra <rahulrudra665@gmail.com>
2024-01-01 08:21:14 +0600

    mvc started

--- ConceptualSessions/MurirTinConceptual Final/src/app.ts
+++ ConceptualSessions/MurirTinConceptual Final/src/app.ts
@@ -1,7 +1,35 @@
-import express from "express";
-const app = express();
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
 app.use(express.json());
+app.use(cors());
+
+const userRouter = express.Router();
+// app.get("/users", userRouter);
+userRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+  const users = [
+    {
+      id: 1,
+      name: "Rahul Rudra",
+      email: "rahul@gmail.com"
+    },
+    {
+      id: 2,
+      name: "Rahuli Rudra",
+      email: "rahuli@gmail.com"
+    }
+  ];
+
+  res.status(200).json({
+    message: "User Fetched Successfully",
+    status: "Success",
+    data: users
+  });
+  next();
+});
+app.use("/api/v1/users", userRouter);
+
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Get Route Done",
--- ConceptualSessions/MurirTinConceptual Final/src/server.ts
+++ ConceptualSessions/MurirTinConceptual Final/src/server.ts
@@ -6,11 +6,6 @@ async function server() {
   try {
     await mongoose.connect(config.database_url_cloud);
 
-    const Cat = mongoose.model("Cat");
-
-    const kitty = new Cat({ name: "Zildjian" });
-    kitty.save().then(() => console.log("meow"));
-
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
