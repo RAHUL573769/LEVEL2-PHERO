5514c35701179c722a464d0fce85e2eb8522d75d
Rahul Rudra <rahulrudra665@gmail.com>
2024-01-04 07:20:25 +0600

    new commit

--- TypeScriptTechnoCrabt/Practises/src/Module-02.ts
+++ TypeScriptTechnoCrabt/Practises/src/Module-02.ts
@@ -32,3 +32,35 @@ console.log(user);
 
 const graph: [x: number, y: number] = [222.5, 12.3];
 console.log(graph);
+const poorUser7: {
+  name: string;
+  addBalance: () => string;
+
+  newBalance: () => number;
+} = {
+  name: "Rahul",
+  addBalance(): string {
+    return "Hello";
+  },
+  newBalance() {
+    return 45;
+  }
+};
+const mentor1: {
+  react: string;
+  prisma: string;
+  cloud: string;
+} = {
+  react: "Mir",
+  prisma: "Salim",
+  cloud: "eraks"
+};
+
+const mentors = {
+  ...mentor1
+};
+
+console.log(mentors);
+
+const { ...mentorsRest } = mentor1;
+console.log("55", mentorsRest);
