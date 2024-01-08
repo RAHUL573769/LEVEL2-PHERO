d5903ab9d70436b35e5abcaae6a50116f0a4b720
Rahul Rudra <rahulrudra665@gmail.com>
2024-01-08 15:25:56 +0600

    commit

--- Redux/projectone/src/Componenets/userinfousinguseReducer.jsx
+++ Redux/projectone/src/Componenets/userinfousinguseReducer.jsx
@@ -10,7 +10,10 @@ export const UserInfowithUseReducer = () => {
     switch (action.type) {
       case "addName":
         return { ...currentState, name: action.payload };
-
+      case "addAge":
+        return { ...currentState, age: action.payload };
+      case "addHobbies":
+        return [ ...currentState, hobbies: [ ...currentState.hobbies,action.payload] ];
       default:
         return currentState;
     }
@@ -23,21 +26,23 @@ export const UserInfowithUseReducer = () => {
   console.log(user);
   return (
     <div>
-      <h1>Form Info</h1>
+      <h1>Form Using Use Reducer</h1>
 
       <input
         className={"border border-purple-300 m-30"}
-        onChange={(e) => setUser({ ...user, name: e.target.value })}
+        // onChange={(e) => setUser({ ...user, name: e.target.value })}
+        onChange={(e) => dispatch({ type: "addName", payload: e.target.value })}
         type="text"
         name="name"
         id="name"
         placeholder="name"
       />
+
       <br />
       <input
         className={"border border-purple-300 m-30"}
         // onChange={(e) => setUser({ ...user, age: e.target.value })}
-        onChange={() => dispatch({ type: "addName" })}
+        onChange={(e) => dispatch({ type: "addAge", payload: e.target.value })}
         type="text"
         name="age"
         id="age"
@@ -47,15 +52,19 @@ export const UserInfowithUseReducer = () => {
       <input
         className={"border border-purple-300 m-30"}
         type="text"
+        // onBlur={(e) =>
+        //   setUser({ ...user, hobbies: [...user.hobbies, e.target.value] })
+        // }
+
         onBlur={(e) =>
-          setUser({ ...user, hobbies: [...user.hobbies, e.target.value] })
+          dispatch({ type: "addHobbies", payload: e.target.value })
         }
         name="hobbies"
         id="hobbies"
         placeholder="hobbies"
       />
       <br />
-      <button type="submit">Submit</button>
+      {/*  */}
     </div>
   );
 };
