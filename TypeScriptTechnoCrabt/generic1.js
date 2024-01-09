fcd3969e68d0a7d9a8033be28162fc43c98144bd
Rahul Rudra <rahulrudra665@gmail.com>
2024-01-09 15:20:14 +0600

    practise done

--- TypeScriptTechnoCrabt/generic1.js
+++ TypeScriptTechnoCrabt/generic1.js
@@ -2,4 +2,16 @@
 {
     let name = ["rahul", "rohit"];
     console.log(name);
+    let any;
+    any;
+    const kgToGram = (value) => {
+        if (typeof value === "number") {
+            return "This is Number";
+        }
+        else {
+            return "This is String";
+        }
+    };
+    const output1 = kgToGram(1234);
+    console.log(output1);
 }
