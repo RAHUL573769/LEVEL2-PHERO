fea3d7fa2c6b91aef3c67f1dbcdacb0ff5669532
Rahul Rudra <rahulrudra665@gmail.com>
2024-01-16 15:30:56 +0600

    database connected

---
+++ FinalProjects/UniversityManagement/process.env.d.ts
@@ -0,0 +1,24 @@
+// declare namespace NodeJs {
+//   export type ProcessEnv = {
+//     PORT: number;
+//     DATABASE_URL_CLOUD: string;
+//     DATABASE_LOCAL: string;
+//   };
+// }
+
+declare namespace NodeJS {
+  export type ProcessEnv = {
+    PORT: number;
+    DATABASE_URL_CLOUD: string;
+    DATABASE_LOCAL: string;
+  };
+}
+
+// declare namespace NodeJS {
+//   export type ProcessEnv = {
+//     PORT: number;
+//     DATABASE_URL_Cloud: string;
+//     DATABASE_URL: string;
+//     NODE_ENV: string;
+//   };
+// }
