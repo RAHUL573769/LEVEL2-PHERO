17b86b8b31e82ee302e9464fd08453993b3d0a43
Rahul Rudra <rahulrudra665@gmail.com>
2024-02-12 09:35:06 +0600

    changes made'

--- FinalProjects/UniversityManagement(Module13STARTED)/L2B2-PH-university-server-first-project-4/src/app/config/index.ts
+++ FinalProjects/UniversityManagement(Module13STARTED)/L2B2-PH-university-server-first-project-4/src/app/config/index.ts
@@ -5,7 +5,8 @@ dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
 export default {
   port: process.env.PORT,
-  database_url: process.env.DATABASE_URL_CLOUD,
+  database_url: process.env.DATABASE_LOCAL,
+
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   default_password: process.env.DEFAULT_PASS,
 };
