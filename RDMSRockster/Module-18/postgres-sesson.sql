98731a2e6ec1de9644310409ea537e67da89efc6
Rahul Rudra <rahulrudra665@gmail.com>
2024-03-19 15:48:37 +0600

    module 17 commit

--- RDMSRockster/Module-18/postgres-sesson.sql
+++ RDMSRockster/Module-18/postgres-sesson.sql
@@ -17,7 +17,7 @@ select * from persons;
 
 CREATE TABLE persons (
   id SERIAL PRIMARY KEY ,  
-    name VARCHAR(50) NOT NULL,
+    name1 VARCHAR(50) NOT NULL,
     age INT,
     profession VARCHAR(100),
     isActive BOOLEAN,
@@ -25,4 +25,28 @@ CREATE TABLE persons (
     ratings NUMERIC(5,2)
 );
 
-INSERT INTO persons (id,name,age,profession,isActive,dob,ratings)  VALUES ( 1,"Rahul Rudra",23,"Student",false,'2021-12-02',4.55)
+INSERT INTO persons (id,name1,age,profession,isActive,dob,ratings)  VALUES ( 2,'Rahul Rudra',23,'Student',false,'2021-12-02',4.55)
+
+ALTER Table persons 
+RENAME to  person3;
+
+
+select * from person3;
+ALTER Table person3 
+RENAME COLUMN dob to dateofbirth
+
+ALTER Table person3 
+ADD COLUMN email VARCHAR(50);
+
+select * from person3;
+
+alter table person3 ADD COLUMN description1 VARCHAR(20);
+
+select * from person3;
+
+
+INSERT INTO person3 (id,name1,age,profession,isActive,dateofbirth,ratings,email,description1)  VALUES ( 3,'Rahul Rudra',23,'Student',false,'2021-12-02',4.55,'rahul@gmail.com','This is Good')
+
+select * from person3;
+
+alter table person3 add CONSTRAINT CHECK_person3_profession UNIQUE(profession)
