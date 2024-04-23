aa46279e37d5dc2a4f9f7900feee4510ecaa48cf
Rahul Rudra <rahulrudra665@gmail.com>
2024-04-23 13:37:13 +0600

    module-5 started

--- NoSqlBackendBraniac/A_PROJECTS_DOING_NOW/first-project-first-project-5/src/app/routes/index.ts
+++ NoSqlBackendBraniac/A_PROJECTS_DOING_NOW/first-project-first-project-5/src/app/routes/index.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { AdminRoutes } from '../modules/Admin/admin.route';
+import { CourseRoutes } from '../modules/Course/course.route';
 import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
@@ -17,6 +19,14 @@ const moduleRoutes = [
     route: StudentRoutes,
   },
   {
+    path: '/faculties',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/admins',
+    route: AdminRoutes,
+  },
+  {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
   },
@@ -27,6 +37,10 @@ const moduleRoutes = [
   {
     path: '/academic-departments',
     route: AcademicDepartmentRoutes,
+  },
+  {
+    path: '/courses',
+    route: CourseRoutes,
   },
 ];
 
